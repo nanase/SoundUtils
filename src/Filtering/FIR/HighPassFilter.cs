15887d8a182f20345970154da3875e7575839bea
Tomona Nanase <nanase@users.noreply.github.com>
2014-01-25 00:19:50 +0900

    HighPassFilterクラスを追加

---
+++ src/Filtering/FIR/HighPassFilter.cs
@@ -0,0 +1,45 @@
+﻿/* SoundUtils
+
+LICENSE - The MIT License (MIT)
+
+Copyright (c) 2014 Tomona Nanase
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+
+namespace SoundUtils.Filtering.FIR
+{
+    public class HighPassFilter : CutoffFilter
+    {
+        #region -- Protected Methods --
+        protected override void GenerateValues(double[] array, int size)
+        {
+            // size は奇数
+
+            int offset = size / 2;
+            double fe_2 = 2.0 * (this.CutoffFrequency / this.SamplingRate);
+            double fe_PI2 = Math.PI * fe_2;
+
+            for (int i = 0, j = -offset; j <= offset; i++, j++)
+                array[i] = SoundMath.Sinc(Math.PI * j) - fe_2 * SoundMath.Sinc(fe_PI2 * j);
+        }
+        #endregion
+    }
+}
