0602bea510fe195d06d92fae958efe9327f57423
Tomona Nanase <nanase@users.noreply.github.com>
2014-02-10 14:25:16 +0900

    FiniteImpulseResponseクラスを追加

---
+++ src/Filtering/FIR/FiniteImpulseResponse.cs
@@ -0,0 +1,66 @@
+﻿/* SoundUtils
+
+LICENSE - The MIT License (MIT)
+
+Copyright (c) 2014 Tomona Nanase
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+
+namespace SoundUtils.Filtering.FIR
+{
+    public abstract class FiniteImpulseResponse : ImpulseResponse
+    {
+        #region -- Public Methods --
+        public double[] Generate(double delta)
+        {
+            return base.Generate(FiniteImpulseResponse.GetFilterSize(this.SamplingRate, delta));
+        }
+
+        public void Generate(double[] array, double delta)
+        {
+            base.Generate(array, FiniteImpulseResponse.GetFilterSize(this.SamplingRate, delta));
+        }
+        #endregion
+
+        #region -- Public Static Methods --
+        public static double GetDelta(double samplingRate, int delayer)
+        {
+            // 奇数で返す
+            if ((delayer & 1) == 0)
+                delayer--;
+
+            return (3.1 / (delayer - 0.5)) * samplingRate;
+        }
+
+        public static int GetFilterSize(double samplingRate, double delta)
+        {
+            delta /= samplingRate;
+            int delayer = (int)(3.1 / delta + 0.5) - 1;
+
+            // 偶数で返す
+            if ((delayer & 1) == 1)
+                delayer++;
+
+            return delayer;
+        }
+        #endregion
+    }
+}
