7d94449d7a3a04ea81456c3fa332223849718c07
Tomona Nanase <nanase@users.noreply.github.com>
2016-12-25 19:55:13 +0900

    :white_check_mark: Windowのテストを追加

--- UnitTest/Math/WindowTest.cs
+++ UnitTest/Math/WindowTest.cs
@@ -14,6 +14,13 @@ namespace UnitTest
         {
             new CaseType(Window.Hanning, new[] { 0.25, 1.0, 0.25 }),
             new CaseType(Window.Hamming, new[] { 0.31, 1.0, 0.31 }),
+            new CaseType(Window.Bartlett, new[] { 1.0 / 3.0, 1.0, 1.0 / 3.0 }),
+            new CaseType(Window.Nuttall, new[] { 0.052558, 1.0, 0.052558 }),
+            new CaseType(Window.Blackman, new[] { 0.13, 1.0, 0.13 }),
+            new CaseType(Window.BlackmanHarris, new[] { 0.055645, 1.0, 0.055645 }),
+            new CaseType(Window.BlackmanNuttall, new[] { 0.0613345, 1.0, 0.0613345 }),
+            new CaseType(Window.FlatTop, new[] { -0.238, 4.64, -0.238 }),
+            new CaseType(Window.Welch, new[] { 5.0 / 9.0, 1.0, 5.0 / 9.0 }),
         };
 
         [Test]
