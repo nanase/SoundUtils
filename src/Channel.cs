73acf8213a3a0b484786ce20a55304eaf070b66e
Tomona Nanase <nanase@users.noreply.github.com>
2017-02-05 13:10:34 +0900

    :shirt: 引数名の省略を修正

--- src/Channel.cs
+++ src/Channel.cs
@@ -32,83 +32,85 @@ namespace SoundUtils
     public static class Channel
     {
         #region -- Public Static Methods --
+
         /// <summary>
         /// 配列を 2 つのチャネルに分割します。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
         /// <param name="source">分割される T 型の配列。</param>
-        /// <param name="lch">L チャネルに分割される T 型配列。</param>
-        /// <param name="rch">R チャネルに分割される T 型配列。</param>
-        public static void Split<T>(T[] source, T[] lch, T[] rch)
+        /// <param name="channelL">L チャネルに分割される T 型配列。</param>
+        /// <param name="channelR">R チャネルに分割される T 型配列。</param>
+        public static void Split<T>(T[] source, T[] channelL, T[] channelR)
         {
             if (source == null)
                 throw new ArgumentNullException(nameof(source));
 
-            Deinterleave(source, lch, rch, source.Length / 2);
+            Deinterleave(source, channelL, channelR, source.Length / 2);
         }
 
         /// <summary>
         /// 2 つのチャネルを 1 つの配列に合成します。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="lch">合成元の L チャネルの T 型配列。</param>
-        /// <param name="rch">合成元の R チャネルの T 型配列。</param>
-        /// <param name="dest">合成先の T 型配列。</param>
-        public static void Join<T>(T[] lch, T[] rch, T[] dest)
+        /// <param name="channelL">合成元の L チャネルの T 型配列。</param>
+        /// <param name="channelR">合成元の R チャネルの T 型配列。</param>
+        /// <param name="destination">合成先の T 型配列。</param>
+        public static void Join<T>(T[] channelL, T[] channelR, T[] destination)
         {
-            if (dest == null)
-                throw new ArgumentNullException(nameof(dest));
+            if (destination == null)
+                throw new ArgumentNullException(nameof(destination));
 
-            Interleave(lch, rch, dest, dest.Length / 2);
+            Interleave(channelL, channelR, destination, destination.Length / 2);
         }
 
         #region Interleave
+
         /// <summary>
         /// 単一の配列と既定の値をインターリーブします。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="src">インターリーブされる T 型配列。</param>
-        /// <param name="dest">インターリーブされた結果が格納されるの T 型配列</param>
+        /// <param name="source">インターリーブされる T 型配列。</param>
+        /// <param name="destination">インターリーブされた結果が格納されるの T 型配列</param>
         /// <param name="count">インターリーブされる配列から読み取られるデータ数。</param>
-        public static void Interleave<T>(T[] src, T[] dest, int count)
+        public static void Interleave<T>(T[] source, T[] destination, int count)
         {
-            Interleave(src, 0, dest, 0, count);
+            Interleave(source, 0, destination, 0, count);
         }
 
         /// <summary>
         /// 単一の配列と既定の値をインターリーブします。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="src">インターリーブされる T 型配列。</param>
+        /// <param name="source">インターリーブされる T 型配列。</param>
         /// <param name="srcOffset">読み取りが開始されるインデックスのオフセット。</param>
-        /// <param name="dest">インターリーブされた結果が格納される T 型配列</param>
+        /// <param name="destination">インターリーブされた結果が格納される T 型配列</param>
         /// <param name="destOffset">書き込みが開始されるインデックスのオフセット。</param>
         /// <param name="count">インターリーブされる配列から読み取られるデータ数。</param>
-        public static void Interleave<T>(T[] src, int srcOffset, T[] dest, int destOffset, int count)
+        public static void Interleave<T>(T[] source, int srcOffset, T[] destination, int destOffset, int count)
         {
-            if (src == null)
-                throw new ArgumentNullException(nameof(src));
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
 
-            if (dest == null)
-                throw new ArgumentNullException(nameof(dest));
+            if (destination == null)
+                throw new ArgumentNullException(nameof(destination));
 
-            if (srcOffset < 0 || srcOffset >= src.Length)
+            if (srcOffset < 0 || srcOffset >= source.Length)
                 throw new ArgumentOutOfRangeException(nameof(srcOffset));
 
-            if (destOffset < 0 || destOffset >= dest.Length)
+            if (destOffset < 0 || destOffset >= destination.Length)
                 throw new ArgumentOutOfRangeException(nameof(destOffset));
 
             if (count < 0)
                 throw new ArgumentOutOfRangeException(nameof(count));
 
-            if (src.Length - srcOffset < count / 2 ||
-                dest.Length - destOffset < count)
+            if (source.Length - srcOffset < count / 2 ||
+                destination.Length - destOffset < count)
                 throw new ArgumentOutOfRangeException(nameof(count));
 
             for (int i = 0, j = srcOffset, k = destOffset; i < count; i++, j++, k++)
             {
-                dest[k] = src[j];
-                dest[++k] = default(T);
+                destination[k] = source[j];
+                destination[++k] = default(T);
             }
         }
 
@@ -116,167 +118,177 @@ namespace SoundUtils
         /// 2つの配列をインターリーブします。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="srcR">1つ目の T 型配列。</param>
-        /// <param name="srcI">2つ目の T 型配列。</param>
-        /// <param name="dest">インタリーブされた結果が格納される T 型配列。</param>
+        /// <param name="sourceR">1つ目の T 型配列。</param>
+        /// <param name="sourceI">2つ目の T 型配列。</param>
+        /// <param name="destination">インタリーブされた結果が格納される T 型配列。</param>
         /// <param name="count">インターリーブされる配列から読み取られるデータ数。</param>
-        public static void Interleave<T>(T[] srcR, T[] srcI, T[] dest, int count)
+        public static void Interleave<T>(T[] sourceR, T[] sourceI, T[] destination, int count)
         {
-            Interleave(srcR, 0, srcI, 0, dest, 0, count);
+            Interleave(sourceR, 0, sourceI, 0, destination, 0, count);
         }
 
         /// <summary>
         /// 2つの配列をインターリーブします。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="srcR">1つ目の T 型配列。</param>
-        /// <param name="srcROffset">1つ目の T 型配列の読み取りが開始されるインデックスのオフセット。</param>
-        /// <param name="srcI">2つ目の T 型配列。</param>
-        /// <param name="srcIOffset">2つ目の T 型配列の読み取りが開始されるインデックスのオフセット。</param>
-        /// <param name="dest">インタリーブされた結果が格納される T 型配列。</param>
-        /// <param name="destOffset">結果の T 型配列の格納が開始されるインデックスのオフセット。</param>
+        /// <param name="sourceR">1つ目の T 型配列。</param>
+        /// <param name="sourceROffset">1つ目の T 型配列の読み取りが開始されるインデックスのオフセット。</param>
+        /// <param name="sourceI">2つ目の T 型配列。</param>
+        /// <param name="sourceIOffset">2つ目の T 型配列の読み取りが開始されるインデックスのオフセット。</param>
+        /// <param name="destination">インタリーブされた結果が格納される T 型配列。</param>
+        /// <param name="destinationOffset">結果の T 型配列の格納が開始されるインデックスのオフセット。</param>
         /// <param name="count">インターリーブされる配列から読み取られるデータ数。</param>
-        public static void Interleave<T>(T[] srcR, int srcROffset, T[] srcI, int srcIOffset, T[] dest, int destOffset, int count)
+        public static void Interleave<T>(T[] sourceR, int sourceROffset, T[] sourceI, int sourceIOffset, T[] destination, int destinationOffset, int count)
         {
-            if (srcR == null)
-                throw new ArgumentNullException(nameof(srcR));
+            if (sourceR == null)
+                throw new ArgumentNullException(nameof(sourceR));
 
-            if (srcI == null)
-                throw new ArgumentNullException(nameof(srcI));
+            if (sourceI == null)
+                throw new ArgumentNullException(nameof(sourceI));
 
-            if (dest == null)
-                throw new ArgumentNullException(nameof(dest));
+            if (destination == null)
+                throw new ArgumentNullException(nameof(destination));
 
-            if (srcROffset < 0 || srcROffset >= srcR.Length)
-                throw new ArgumentOutOfRangeException(nameof(srcROffset));
+            if (sourceROffset < 0 || sourceROffset >= sourceR.Length)
+                throw new ArgumentOutOfRangeException(nameof(sourceROffset));
 
-            if (srcIOffset < 0 || srcIOffset >= srcI.Length)
-                throw new ArgumentOutOfRangeException(nameof(srcIOffset));
+            if (sourceIOffset < 0 || sourceIOffset >= sourceI.Length)
+                throw new ArgumentOutOfRangeException(nameof(sourceIOffset));
 
-            if (destOffset < 0 || destOffset >= dest.Length)
-                throw new ArgumentOutOfRangeException(nameof(destOffset));
+            if (destinationOffset < 0 || destinationOffset >= destination.Length)
+                throw new ArgumentOutOfRangeException(nameof(destinationOffset));
 
             if (count < 0)
                 throw new ArgumentOutOfRangeException(nameof(count));
 
-            if (srcR.Length - srcROffset < count / 2 ||
-                srcI.Length - srcIOffset < count / 2 ||
-                dest.Length - destOffset < count)
+            if (sourceR.Length - sourceROffset < count / 2 ||
+                sourceI.Length - sourceIOffset < count / 2 ||
+                destination.Length - destinationOffset < count)
                 throw new ArgumentOutOfRangeException(nameof(count));
 
-            for (int i = 0, j = srcROffset, k = srcIOffset, l = destOffset; i < count; i++, j++, k++)
+            for (int i = 0, j = sourceROffset, k = sourceIOffset, l = destinationOffset; i < count; i++, j++, k++)
             {
-                dest[l++] = srcR[j];
-                dest[l++] = srcI[k];
+                destination[l++] = sourceR[j];
+                destination[l++] = sourceI[k];
             }
         }
+
         #endregion
 
         #region Deinterleave
+
         /// <summary>
         /// 配列のインターリーブを解除し、単一の配列に格納します。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="src">インターリーブされた入力となる T 型配列。</param>
-        /// <param name="dest">インターリーブ解除の結果が格納される T 型配列。</param>
+        /// <param name="source">インターリーブされた入力となる T 型配列。</param>
+        /// <param name="destination">インターリーブ解除の結果が格納される T 型配列。</param>
         /// <param name="count">読み取り元配列のデータ数。</param>
-        public static void Deinterleave<T>(T[] src, T[] dest, int count)
+        public static void Deinterleave<T>(T[] source, T[] destination, int count)
         {
-            Deinterleave(src, 0, dest, 0, count);
+            Deinterleave(source, 0, destination, 0, count);
         }
 
         /// <summary>
         /// 配列のインターリーブを解除し、単一の配列に格納します。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="src">インターリーブされた入力となる T 型配列。</param>
-        /// <param name="srcOffset">T 型配列の読み取りが開始されるインデックスのオフセット。</param>
-        /// <param name="dest">インターリーブ解除の結果が格納される T 型配列。</param>
-        /// <param name="destOffset">結果の T 型配列の格納が開始されるインデックスのオフセット。</param>
+        /// <param name="source">インターリーブされた入力となる T 型配列。</param>
+        /// <param name="sourceOffset">T 型配列の読み取りが開始されるインデックスのオフセット。</param>
+        /// <param name="destination">インターリーブ解除の結果が格納される T 型配列。</param>
+        /// <param name="destinationOffset">結果の T 型配列の格納が開始されるインデックスのオフセット。</param>
         /// <param name="count">読み取り元配列のデータ数。</param>
-        public static void Deinterleave<T>(T[] src, int srcOffset, T[] dest, int destOffset, int count)
+        public static void Deinterleave<T>(T[] source, int sourceOffset, T[] destination, int destinationOffset, int count)
         {
-            if (src == null)
-                throw new ArgumentNullException(nameof(src));
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
 
-            if (dest == null)
-                throw new ArgumentNullException(nameof(dest));
+            if (destination == null)
+                throw new ArgumentNullException(nameof(destination));
 
-            if (srcOffset < 0 || srcOffset >= src.Length)
-                throw new ArgumentOutOfRangeException(nameof(srcOffset));
+            if (sourceOffset < 0 || sourceOffset >= source.Length)
+                throw new ArgumentOutOfRangeException(nameof(sourceOffset));
 
-            if (destOffset < 0 || destOffset >= dest.Length)
-                throw new ArgumentOutOfRangeException(nameof(destOffset));
+            if (destinationOffset < 0 || destinationOffset >= destination.Length)
+                throw new ArgumentOutOfRangeException(nameof(destinationOffset));
 
             if (count < 0)
                 throw new ArgumentOutOfRangeException(nameof(count));
 
-            if (src.Length - srcOffset < count ||
-                (dest.Length - destOffset) * 2 < count)
+            if (source.Length - sourceOffset < count ||
+                (destination.Length - destinationOffset) * 2 < count)
                 throw new ArgumentOutOfRangeException(nameof(count));
 
-            for (int i = 0, j = srcOffset, k = destOffset; i < count; i++, j += 2, k++)
-                dest[k] = src[j];
+            for (int i = 0, j = sourceOffset, k = destinationOffset; i < count; i++, j += 2, k++)
+                destination[k] = source[j];
         }
 
         /// <summary>
         /// 配列のインターリーブを解除し、2つの配列に格納します。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="src">インターリーブされた入力となる T 型配列。</param>
-        /// <param name="destR">インターリーブ解除の結果が格納される 1つ目の T 型配列。</param>
-        /// <param name="destI">インターリーブ解除の結果が格納される 2つ目の T 型配列。</param>
+        /// <param name="source">インターリーブされた入力となる T 型配列。</param>
+        /// <param name="destinationR">インターリーブ解除の結果が格納される 1つ目の T 型配列。</param>
+        /// <param name="destinationI">インターリーブ解除の結果が格納される 2つ目の T 型配列。</param>
         /// <param name="count">読み取り元配列のデータ数。</param>
-        public static void Deinterleave<T>(T[] src, T[] destR, T[] destI, int count)
+        public static void Deinterleave<T>(T[] source, T[] destinationR, T[] destinationI, int count)
         {
-            Deinterleave(src, 0, destR, 0, destI, 0, count);
+            Deinterleave(source, 0, destinationR, 0, destinationI, 0, count);
         }
 
         /// <summary>
         /// 配列のインターリーブを解除し、2つの配列に格納します。
         /// </summary>
         /// <typeparam name="T">配列のデータ型。</typeparam>
-        /// <param name="src">インターリーブされた入力となる T 型配列。</param>
-        /// <param name="srcOffset"></param>
-        /// <param name="destR">インターリーブ解除の結果が格納される 1つ目の T 型配列。</param>
-        /// <param name="destROffset">1つ目の T 型配列の格納が開始されるインデックスのオフセット。</param>
-        /// <param name="destI">インターリーブ解除の結果が格納される 2つ目の T 型配列。</param>
-        /// <param name="destIOffset">2つ目の T 型配列の格納が開始されるインデックスのオフセット。</param>
+        /// <param name="source">インターリーブされた入力となる T 型配列。</param>
+        /// <param name="sourceOffset"></param>
+        /// <param name="destinationR">インターリーブ解除の結果が格納される 1つ目の T 型配列。</param>
+        /// <param name="destinationROffset">1つ目の T 型配列の格納が開始されるインデックスのオフセット。</param>
+        /// <param name="destinationI">インターリーブ解除の結果が格納される 2つ目の T 型配列。</param>
+        /// <param name="destinationIOffset">2つ目の T 型配列の格納が開始されるインデックスのオフセット。</param>
         /// <param name="count">読み取り元配列のデータ数。</param>
-        public static void Deinterleave<T>(T[] src, int srcOffset, T[] destR, int destROffset, T[] destI, int destIOffset, int count)
+        public static void Deinterleave<T>(
+            T[] source,
+            int sourceOffset,
+            T[] destinationR, 
+            int destinationROffset,
+            T[] destinationI,
+            int destinationIOffset, 
+            int count)
         {
-            if (src == null)
-                throw new ArgumentNullException(nameof(src));
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
 
-            if (destR == null)
-                throw new ArgumentNullException(nameof(destR));
+            if (destinationR == null)
+                throw new ArgumentNullException(nameof(destinationR));
 
-            if (destI == null)
-                throw new ArgumentNullException(nameof(destI));
+            if (destinationI == null)
+                throw new ArgumentNullException(nameof(destinationI));
 
-            if (srcOffset < 0 || srcOffset >= src.Length)
-                throw new ArgumentOutOfRangeException(nameof(srcOffset));
+            if (sourceOffset < 0 || sourceOffset >= source.Length)
+                throw new ArgumentOutOfRangeException(nameof(sourceOffset));
 
-            if (destROffset < 0 || destROffset >= destR.Length)
-                throw new ArgumentOutOfRangeException(nameof(destROffset));
+            if (destinationROffset < 0 || destinationROffset >= destinationR.Length)
+                throw new ArgumentOutOfRangeException(nameof(destinationROffset));
 
-            if (destIOffset < 0 || destIOffset >= destI.Length)
-                throw new ArgumentOutOfRangeException(nameof(destIOffset));
+            if (destinationIOffset < 0 || destinationIOffset >= destinationI.Length)
+                throw new ArgumentOutOfRangeException(nameof(destinationIOffset));
 
             if (count < 0)
                 throw new ArgumentOutOfRangeException(nameof(count));
 
-            if (src.Length - srcOffset < count * 2 ||
-                destR.Length - destROffset < count ||
-                destI.Length - destIOffset < count)
+            if (source.Length - sourceOffset < count * 2 ||
+                destinationR.Length - destinationROffset < count ||
+                destinationI.Length - destinationIOffset < count)
                 throw new ArgumentOutOfRangeException(nameof(count));
 
-            for (int i = 0, j = srcOffset, k = destROffset, l = destIOffset; i < count; i++, k++, l++)
+            for (int i = 0, j = sourceOffset, k = destinationROffset, l = destinationIOffset; i < count; i++, k++, l++)
             {
-                destR[k] = src[j++];
-                destI[l] = src[j++];
+                destinationR[k] = source[j++];
+                destinationI[l] = source[j++];
             }
         }
+
         #endregion
         #endregion
     }
